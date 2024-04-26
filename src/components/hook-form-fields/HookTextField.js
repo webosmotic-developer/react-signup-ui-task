fc38537b8d77135f0d99ff41eec760ccc41bf510
Kanika <kanika.webosmotic@gmail.com>
2024-04-26 11:11:58 +0530

    Added icons and managed password show hide state

--- src/components/hook-form-fields/HookTextField.js
+++ src/components/hook-form-fields/HookTextField.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PropTypes from 'prop-types';
 import { useController } from 'react-hook-form';
+import { EyeOffOutline, EyeOutline } from 'react-ionicons';
 
 /**
  * Common component for text input
@@ -9,23 +10,62 @@ import { useController } from 'react-hook-form';
 
 export const HookTextField = (props) => {
   const { field, fieldState, formState } = useController(props);
-
+  const { type } = props;
+  const [showPassword, setShowPassword] = useState(false);
+  console.log({ type });
   return (
     <>
       <p className="text-base">{props.label}</p>
-      <input
-        className="border-inputBorder mb-3 mt-1 appearance-none border rounded-md py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 w-full text-base"
-        {...field}
-        {...props}
-      />
-      {(fieldState?.isTouched || formState?.isSubmitted) &&
-        fieldState?.error?.message}
-      {(fieldState?.isTouched || formState?.isSubmitted) &&
-        Boolean(fieldState?.error)}
+
+      <div className="flex items-center justify-end mb-3 mt-1">
+        <input
+          className={`border-inputBorder appearance-none border rounded-md py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 w-full text-base ${type === 'password' ? 'pr-11' : 'pr-2'}`}
+          {...field}
+          {...props}
+          type={
+            type === 'password'
+              ? showPassword
+                ? 'text'
+                : 'password'
+              : props?.type
+          }
+        />
+        {type === 'password' ? (
+          showPassword ? (
+            <EyeOffOutline
+              color={'#292D32'}
+              width={'24px'}
+              height={'24px'}
+              className="absolute mr-3"
+              onClick={() => setShowPassword((prev) => !prev)}
+            />
+          ) : (
+            <EyeOutline
+              color={'#292D32'}
+              width={'24px'}
+              height={'24px'}
+              className="absolute mr-3"
+              onClick={() => setShowPassword((prev) => !prev)}
+            />
+          )
+        ) : null}
+      </div>
+
+      {fieldState?.isTouched || formState?.isSubmitted ? (
+        <p className="text-14 leading-5 text-helperGray mb-3">
+          {fieldState?.error?.message}
+        </p>
+      ) : null}
+      {fieldState?.isTouched || formState?.isSubmitted ? (
+        <p className="text-14 leading-5 text-helperGray mb-3">
+          {Boolean(fieldState?.error)}
+        </p>
+      ) : null}
     </>
   );
 };
 
 HookTextField.propTypes = {
   label: PropTypes.any,
+  type: PropTypes.string,
 };
